4b806515cb508fc8ea9566e77e696ceff438b776
DenisSeroshtan <haron_777@mail.ru>
2018-07-01 03:38:27 +0500

    add five latest repos

--- ui.js
+++ ui.js
@@ -37,7 +37,7 @@ class UI {
 
     repos.forEach(repo => {
       
-      output = `
+      output += `
         <div class = "card card-body mb-2">
           <div class = "row">
             <div class = "col-md-6"> 
