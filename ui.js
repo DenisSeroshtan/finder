773726f298a5ae7a4d08925300fb6e586748d7ac
DenisSeroshtan <haron_777@mail.ru>
2018-07-01 03:39:31 +0500

    fix

--- ui.js
+++ ui.js
@@ -46,7 +46,7 @@ class UI {
             <div class = "col-md-6"> 
               <span class = "badge badge-primary"> stars: ${repo.stargazers_count}</span>
                 <span class = "badge badge-secondary"> watchers: ${repo.watchers_count} </span>
-                <span class = "badge badge-success"> forks: ${repo.forms_count}</span>
+                <span class = "badge badge-success"> forks: ${repo.forks_count}</span>
             </div> 
           </div> 
         </div>
